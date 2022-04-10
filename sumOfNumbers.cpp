9980791e52b65ddc8f18430fda0980c077678488
Chayank Das <chayank0388.be21@chitkara.edu.in>
2022-04-10 12:01:23 +0530

    added three C++ files

---
+++ sumOfNumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    cout<<"\n Sum of two numbers :\n";
+    cout<<"------\n";
+    cout<<"Input 1st no. :";
+    cin>>num1;
+    cout<<"Input 2nd no. :";
+    cin>>num2;
+    sum=num1 + num2;
+    cout<<"the sum is :"<< sum << endl;
+    cout<< endl; 
+    return 0;
+
